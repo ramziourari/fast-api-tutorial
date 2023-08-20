d9a6e71b6485843e1704013813102f81fa68044c
Ramzi Ourari <Ramzi.Ourari@d-fine.com>
2023-08-20 22:19:35 +0200

    fix import

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,8 +1,8 @@
-import models, schemas
+from app import models, schemas
 from sqlalchemy import exc
 from fastapi import status, Depends, HTTPException, APIRouter
 from typing import Optional
-from database import get_db
+from app.database import get_db
 from sqlalchemy.orm import Session
 from typing import List
 import oauth2
